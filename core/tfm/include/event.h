0dc8921ecd5b4050afcab4d74fecc24f2820a675
Martin Ertsaas <martiert@gmail.com>
2012-11-06 08:02:10 +0100

    tfm: Added the start of a event interface.
    Signed-off-by: Martin Ertsaas <martiert@gmail.com>

---
+++ core/tfm/include/event.h
@@ -0,0 +1,15 @@
+#ifndef TANDFEVENT_H_INCLUDED
+#define TANDFEVENT_H_INCLUDED
+
+#include <string>
+
+namespace tfm
+{
+  class Event
+  {
+    public:
+      virtual ~Event() {}
+  };
+}
+
+#endif // TANDFEVENT_H_INCLUDED

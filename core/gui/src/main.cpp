9afcbd2ebe92c1c4397a7e68c11b68b738e6c066
Martin Ertsaas <martiert@gmail.com>
2013-02-24 18:02:27 +0100

    gui: Change title of application.

--- core/gui/src/main.cpp
+++ core/gui/src/main.cpp
@@ -5,10 +5,9 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    Q_INIT_RESOURCE(resources);
 
     MainWindow window;
-    window.setWindowTitle("Enter your age");
+    window.setWindowTitle("Track and Field Management");
 
     window.show();
 
